ca750dad3b98c2ff23916f5ef8db92f1da136847
Brett Holland <brett.holland@gmail.com>
2018-04-30 13:01:43 -0400

    added height conversion

--- app/javascript/components/Player.js
+++ app/javascript/components/Player.js
@@ -1,30 +1,92 @@
-import React from "react";
+import React from 'react';
 
 const Player = props => {
-	return (
-		<div className="row no-gutters pb-4 mb-4 border-bottom">
-			<div className="col-auto pr-3 h5 secondary-text">{props.rank}.</div>
-			<div className="col">
-				<div className="row align-items-center">
-					<div className="col-sm-6 col-md-8">
-						<div className="row">
-							<div className="col-12 h5">{props.name}</div>
-							<div className="col-sm-12 col-md-auto secondary-text">{props.team}</div>
-							<div className="col-sm-12 col-md-auto secondary-text">{props.position}</div>
-							<div className="col-sm-12 col-md-auto secondary-text">
-								{props.height} inches
-							</div>
-							<div className="col-sm-12 col-md-auto secondary-text">{props.weight} lbs</div>
-						</div>
-					</div>
-					<div className="col-sm-6 col-md-4 text-right">
-						<span className="votes rounded">{props.votes}</span>
-						<i className="fas fa-chevron-up ml-2 rounded" onClick={props.handleUpVoteClick} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  let dataTarget = `#${props.id}`;
+  return (
+    <div className="row no-gutters pb-4 mb-4 border-bottom">
+      <div className="col-auto pr-3 h5 secondary-text player-rank">{props.rank}.</div>
+      <div className="col">
+        <div className="row align-items-center">
+          <div className="col-sm-6 col-md-8">
+            <div className="row">
+              <div className="col-12 h5 player-name" data-toggle="modal" data-target={dataTarget}>
+                {props.name}
+              </div>
+              <div className="col-sm-12 col-md-auto secondary-text">{props.team}</div>
+              <div className="col-sm-12 col-md-auto secondary-text">{props.position}</div>
+              <div className="col-sm-12 col-md-auto secondary-text">{props.height}</div>
+              <div className="col-sm-12 col-md-auto secondary-text">{props.weight} lbs</div>
+            </div>
+          </div>
+          <div className="col-sm-6 col-md-4 text-right">
+            <span className="votes rounded">{props.votes}</span>
+            <i className="fas fa-chevron-up ml-2 rounded" onClick={props.handleUpVoteClick} />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="modal fade"
+        id={props.id}
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby={props.id}
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                {props.name}
+              </h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <table className="table table-borderless">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
--- app/javascript/components/Players.js
+++ app/javascript/components/Players.js
@@ -1,96 +1,103 @@
-import React, { Component } from "react";
-import Player from "./Player";
+import React, { Component } from 'react';
+import Player from './Player';
 
 class HomeContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			players: []
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: []
+    };
     this.handleUpVote = this.handleUpVote.bind(this);
-	}
+    this.convertInches = this.convertInches.bind(this);
+  }
 
-	componentDidMount() {
-		fetch("/api/v1/players", { credentials: "same-origin" })
-			.then(response => {
-				if (response.ok) {
-					return response;
-				} else {
-					let errorMessage = `${response.status} (${response.statusText})`,
-						error = new Error(errorMessage);
-					throw error;
-				}
-			})
-			.then(response => response.json())
-			.then(body => {
-				this.setState({
-					players: body.players
-				});
-			})
-			.catch(error => console.error(`Error in fetch: ${error.message}`));
-	}
-
-  handleUpVote(id, votes) {
-		let newVoteCount = votes + 1;
-		let playerId = id;
-		let submission = {
-			id: playerId,
-			votes: newVoteCount
-		};
-    fetch(`/api/v1/players/${submission.id}`, {
-        credentials: "same-origin",
-        method: "PATCH",
-        body: JSON.stringify(submission),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
+  componentDidMount() {
+    fetch('/api/v1/players', { credentials: 'same-origin' })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+          throw error;
         }
       })
-        .then(response => {
-          if (response.ok) {
-            return response;
-          } else {
-            let errorMessage = `${response.status} (${response.statusText})`,
-              error = new Error(errorMessage);
-            throw error;
-          }
-        })
-        .then(response => response.json())
-        .then(body => {
-          this.setState({ players: body.players });
-        })
-        .catch(error => console.error(`Error in fetch: ${error.message}`));
-	}
+      .then(response => response.json())
+      .then(body => {
+        this.setState({
+          players: body.players
+        });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
 
+  handleUpVote(id, votes) {
+    let newVoteCount = votes + 1;
+    let playerId = id;
+    let submission = {
+      id: playerId,
+      votes: newVoteCount
+    };
+    fetch(`/api/v1/players/${submission.id}`, {
+      credentials: 'same-origin',
+      method: 'PATCH',
+      body: JSON.stringify(submission),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+          throw error;
+        }
+      })
+      .then(response => response.json())
+      .then(body => {
+        this.setState({ players: body.players });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
 
+  convertInches(inches) {
+    let feetFromInches = Math.floor(inches / 12);
+    let inchesRemainder = inches % 12;
 
-	render() {
-		let { players } = this.state;
-		let rank = 0;
-		let showPosts = players.map(player => {
-			rank = rank + 1;
+    return feetFromInches + "'-" + inchesRemainder + '"';
+  }
+
+  render() {
+    let { players } = this.state;
+    let rank = 0;
+    let showPosts = players.map(player => {
+      rank = rank + 1;
       let handleUpVoteClick = () => {
-				this.handleUpVote(player.id, player.votes);
-			};
-			return (
-				<Player
-					key={player.id}
-					id={player.id}
-					rank={rank}
-					name={player.name}
-					position={player.position}
-					team={player.team}
-					height={player.height}
-					weight={player.weight}
-					year={player.votes}
-					birth={player.birth}
-					votes={player.votes}
+        this.handleUpVote(player.id, player.votes);
+      };
+      let height = this.convertInches(player.height);
+      return (
+        <Player
+          key={player.id}
+          id={player.id}
+          rank={rank}
+          name={player.name}
+          position={player.position}
+          team={player.team}
+          height={height}
+          weight={player.weight}
+          year={player.votes}
+          birth={player.birth}
+          votes={player.votes}
           handleUpVoteClick={handleUpVoteClick}
-				/>
-			);
-		});
-		return <div className="container mt-5">{showPosts}</div>;
-	}
+        />
+      );
+    });
+    return <div className="container mt-5">{showPosts}</div>;
+  }
 }
 
 export default HomeContainer;
